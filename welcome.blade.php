dbea98b2917c0b350ff241e934232ce6e8b07620
Alisa <79295091+alisamuti17@users.noreply.github.com>
2021-03-09 15:09:23 +0700

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> CV Design</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript" src="http://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+    <div class="resume">
+        <div class="resume_left">
+            <div class="resume_profile">
+                <img src="foto.jpg" alt="profile_pic">
+            </div>
+            <div class="resume_content">
+                <div class="resume_item resume_info">
+                    <div class="title">
+                        contact
+                    </div>
+                    <ul>
+                        <li>
+                            <div class="icon"><i class="fas fa-map-signs"></i></div>
+                            <div class="data">
+                                warudoyong<br/> 
+                            </div>
+                        </li>
+                        <li>
+                            <div class="icon"><i class="fas fa-mobile"></i></div>
+                            <div class="data">
+                                0895607680904
+                            </div>
+                        </li>
+                        <li>
+                            <div class="icon"><i class="fas fa-envelope"></i></div>
+                            <div class="data">
+                                alisamuti@gmail.com
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="resume_item resume_social">
+                        <div class="title">
+                            Social
+                    </div>
+                    <ul>
+                        <li>
+                            <div class="icon">
+                                <i class="fab fa-facebook-square"></i>
+                            </div>
+                            <div class="data">
+                                <p class="semi_bold">
+                                    Facebook
+                                </p>
+                                <p>alisa@facebook</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <i class="fab fa-twitter-square"></i>
+                            </div>
+                            <div class="data">
+                                <p class="semi_bold">
+                                    Twiter
+                                </p>
+                                <p>alisamuti@Twiter</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <i class="fab fa-instagram"></i>
+                            </div>
+                            <div class="data">
+                                <p class="semi_bold">
+                                    Instagram
+                                </p>
+                                <p>alllisa_u@instagram</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <i class="fab fa-line"></i>
+                            </div>
+                            <div class="data">
+                                <p class="semi_bold">
+                                    Line
+                                </p>
+                                <p>alisamuti17@line</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="resume_right">
+            <div class="resume_item resume_detail">
+                <div class="title">
+                    <p class="bold">PERSONAL DETAIL</p>
+                </div>
+                <ul>
+                    <li>
+                        <div class="date"></div>
+                        <div class="info">
+                            <table>
+                            <tr>
+                                <td class="text">Nama</td>
+                                <td>:</td>
+                                <td>Alisa Muti Sugianti</td>    
+                            </tr>
+                            <tr>
+                                <td class="text">Gender</td>
+                                <td>:</td>
+                                <td>Famale</td>
+                            </tr>
+                            <tr>
+                                <td class="text">Majoring</td>
+                                <td>:</td>
+                                <td>Multimedia</td>
+                            </tr>
+                            <tr>
+                                <td class="text">Religion</td>
+                                <td>:</td>
+                                <td>islam</td>
+                            </tr>
+                            <tr>
+                                <td class="text">Adreess</td>
+                                <td>:</td>
+                                <td>jl.pelabuhan 2 Gg.murni,rt5/rw1,Warudoyong</td>
+                            </tr>
+                            <tr>
+                                <td class="text">Citizenship</td>
+                                <td>:</td>
+                                <td>Indonesia</td>
+                            </tr>
+                        </table>
+                        </div>
+                </ul>
+            </div>
+            <div class="resume_item resume_education">
+                <div class="title">
+                    <p class="bold">Formal Education</p>
+                </div>
+                <ul>
+                    <li>
+                        <div class="date">2007-2008</div>
+                        <div class="info">
+                            <p class="semi-bold">TK AL-BUKHORI</p>
+                            <P>jl.parigi,warudoyong,kec.warudoyong</P>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="date">2009-2016</div>
+                        <div class="info">
+                            <p class="semi-bold">MIS HAYATUL ISLAMIYAH</p>
+                            <P>jl.pelabuhan II GG.murni no.31,kel.dayeuhluhur,warudoyong</P>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="date">2017-2019</div>
+                        <div class="info">
+                            <p class="semi-bold">SMP 11 KOTA SUKABUMI</p>
+                            <p>nyomplong,kec.warudoyong,kota sukabumi</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="date">2019-present</div>
+                        <div class="info">
+                            <p class="semi-bold">SMKN2 KOTA SUKABUMI</p>
+                            <P>jl.pelabuhan II.No.1 Rt1/Rw.6,Cikondang,kec.Citamiang</P>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="resume_item resume_hobby">
+                <div class="title">
+                    <p class="bold">Hobby</p>
+                </div>
+                <ul>
+                    <li><i class="fas fa-book" aria-hidden="true"></i></li>
+                    <li><i class="fas fa-gamepad" aria-hidden="true"></i></li>
+                    <li><i class="fas fa-music" aria-hidden="true"></i></li>
+                    <li><i class="fas fa-swimming-pool" aria-hidden="true"></i></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
